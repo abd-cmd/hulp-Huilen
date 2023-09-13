ee16f0439e9964ad6a8e7569073e1d4b9d48be2f
JtdeGraaf <jt.graaf@gmail.com>
2023-09-13 15:33:21 +0200

    Optie om student in te schrijven voor opleiding in domein

--- monoliet/src/main/java/nl/hu/inno/humc/monoliet/domain/BSA.java
+++ monoliet/src/main/java/nl/hu/inno/humc/monoliet/domain/BSA.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Entity
 public class BSA {
 
@@ -12,22 +15,30 @@ public class BSA {
     private Long id;
     private int minVerplichteStudiePunten;
     private int behaaldeStudiepunten;
+    private LocalDate ingangsDatum;
 
 
     protected BSA(){}
-    public BSA(int minVerplichteStudiePunten) {
+    public BSA(int minVerplichteStudiePunten, LocalDate ingangsDatum) {
         this.minVerplichteStudiePunten = minVerplichteStudiePunten;
+        this.ingangsDatum = ingangsDatum;
         this.behaaldeStudiepunten = 0;
     }
 
     public void voegStudiePuntenToe(Integer studiePunten){
         if(studiePunten == null) throw new RuntimeException();
-        if(studiePunten < 0) throw new RuntimeException("");
+        if(studiePunten < 0) throw new RuntimeException();
+        if(this.ingangsDatum.isBefore(LocalDate.now().minusYears(2))){
+            throw new RuntimeException("Ingangsdatum is ouder dan 2 jaar, kan geen studiepunten meer toevoegen");
+        }
 
         this.behaaldeStudiepunten += studiePunten;
     }
 
     public boolean getHuidigAdvies(){
-        return this.behaaldeStudiepunten >= this.minVerplichteStudiePunten;
+        if(this.ingangsDatum.isAfter(LocalDate.now().minusYears(1))) {
+            return this.behaaldeStudiepunten >= this.minVerplichteStudiePunten;
+        }
+        else return this.behaaldeStudiepunten == this.minVerplichteStudiePunten;
     }
 }
--- monoliet/src/main/java/nl/hu/inno/humc/monoliet/domain/Student.java
+++ monoliet/src/main/java/nl/hu/inno/humc/monoliet/domain/Student.java
@@ -3,7 +3,8 @@ package nl.hu.inno.humc.monoliet.domain;
 import jakarta.persistence.*;
 import nl.hu.inno.humc.monoliet.domain.persoonsgegevens.PersoonsGegevens;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 
 @Entity
 public class Student {
@@ -25,14 +26,41 @@ public class Student {
 
     protected Student(){}
 
-    public Student(PersoonsGegevens persoonsGegevens, Vooropleiding vooropleiding, BSA bsa, String opleiding){
-
+    public Student(PersoonsGegevens persoonsGegevens, Vooropleiding vooropleiding){
+        if(persoonsGegevens == null || vooropleiding == null){
+            throw new RuntimeException();
+        }
 
         this.persoonsGegevens = persoonsGegevens;
         this.vooropleiding = vooropleiding;
-        this.studieAdvies = bsa;
-        this.opleiding = opleiding;
     }
+
+    public void schrijfInVoorOpleiding(String opleiding){
+
+        if(isStudentToegestaan()){
+            if(this.studieAdvies == null){
+                this.studieAdvies = new BSA(30, LocalDate.now()); // TODO attributen afleiden van Opleiding object, wanneer deze bestaat.
+            }
+            this.opleiding = opleiding;
+        }
+    }
+
+    private boolean isStudentToegestaan() {
+        if(this.vooropleiding == Vooropleiding.ANDERE) return false;
+        if(this.vooropleiding == Vooropleiding.HAVO || vooropleiding == Vooropleiding.VWO || vooropleiding == Vooropleiding.MBO){
+
+            // Als bsa null is is deze nog nooit aangemaakt
+            if(this.studieAdvies == null){
+                return true;
+            }
+
+            return this.studieAdvies.getHuidigAdvies();
+
+        }
+        throw new RuntimeException();
+    }
+
+
 
 
 
