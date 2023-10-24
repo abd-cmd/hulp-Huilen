729374866a5ff4f8ff5a7e55b6f02e3598a9dbea
JtdeGraaf <jt.graaf@gmail.com>
2023-10-24 19:10:18 +0200

    opleiding service

---
+++ opleiding/src/main/java/nl/hu/inno/humc/opleiding/OpleidingApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.inno.humc.opleiding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpleidingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpleidingApplication.class, args);
+    }
+
+}
